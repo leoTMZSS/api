07aac880e977947d781932c1db0062ed3ab94ff2
leoTMZSS <ltamarozziss@gmail.com>
2026-01-15 08:54:04 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
